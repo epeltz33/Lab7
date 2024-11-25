1f3f3df32d6bf4c92f6bbc3ffd22b68b8846c6bf
epeltz33 <epeltz@optonline.net>
2024-11-25 17:22:52 -0500

    commit just to show code

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+    private readonly Dictionary<string, (string imageSource, string fact)> vehicleData;
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        // Initialize the dictionary with vehicle data
+        vehicleData = new Dictionary<string, (string imageSource, string fact)>
+        {
+            { "Car", ("car.png", "Cars were invented in the late 1800s! The first practical automobile was created by Karl Benz in 1885.") },
+            { "Motorcycle", ("motorcycle.png", "The first motorcycle was created by Gottlieb Daimler in 1885. It was essentially a wooden bicycle with a gasoline engine!") },
+            { "Bicycle", ("bicycle.png", "The modern bicycle design was introduced in the 1880s. It's the most energy-efficient form of human transport!") }
+        };
+    }
+
+    private Void InitializeComponent()
+    {
+        throw new NotImplementedException();
+    }
+
+    private void OnVehicleSelected(object sender, EventArgs e)
+    {
+        if (vehiclePicker.SelectedItem is string selectedVehicle && 
+            vehicleData.TryGetValue(selectedVehicle, out var vehicleInfo))
+        {
+            // Update the UI with the selected vehicle information
+            vehicleImage.Source = vehicleInfo.imageSource;
+            vehicleFactLabel.Text = vehicleInfo.fact;
+
+            // Add a subtle animation to make the updates more engaging
+            uint animationLength = 500;
+            vehicleImage.Opacity = 0;
+            vehicleFactLabel.Opacity = 0;
+
+            vehicleImage.FadeTo(1, animationLength);
+            vehicleFactLabel.FadeTo(1, animationLength);
+
+            // Announce the change for accessibility
+            SemanticScreenReader.Announce($"Selected {selectedVehicle}. {vehicleInfo.fact}");
+        }
+    }
+}
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,25 @@
+﻿using MauiApp1;
+using Microsoft.Extensions.Logging;
+
+namespace VehicleWorldMAUI;
+
+public static class MauiProgram
+{
+    public static MauiProgram CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
